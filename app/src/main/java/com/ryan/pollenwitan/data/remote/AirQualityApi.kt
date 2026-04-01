191c92d2251183628a09dfb7a388dab21ad9a404
Ryan Buttery <21200135+ryan-buttery@users.noreply.github.com>
2026-04-01 09:36:25 +0200

    added missing lib

--- app/src/main/java/com/ryan/pollenwitan/data/remote/AirQualityApi.kt
+++ app/src/main/java/com/ryan/pollenwitan/data/remote/AirQualityApi.kt
@@ -4,6 +4,7 @@ import com.ryan.pollenwitan.data.remote.dto.AirQualityResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
